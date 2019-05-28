f9d415b5d6f5e7615ca6233b1d1647de61b1a35e
Maksim Zhbanov <maksim.zhbanov@accenture.com>
2019-05-28 14:31:47 +0300

    Исправил expansionMap на мутабельный

--- src/functional/expansionMap.js
+++ src/functional/expansionMap.js
@@ -1,7 +1,7 @@
 import elements from '../data/elements';
 
 /**
- * реализация возможности увеличения карты с возможной заменой массива элементов на переданный
+ * Реализация возможности увеличения карты с возможной заменой массива элементов на переданный
  *
  * @param {Object} targetCoordinate - координаты точки для смещений
  * @param {Array} dx - массив смещений для координаты x
@@ -9,87 +9,63 @@ import elements from '../data/elements';
  * @param {Array} map - трехмерный массив объектов карты, которую необходимо увеличить
  * @param {Object} hero - объект героя, координаты которого необходимо менять в зависсимости от расширения
  * @param {Object} replacement - объект, который необходимо сгенерировать на всех координатах смещения
- * @param {Array} elementsForReplace - массив объектов, которые будут заменены на элемент replacement
  * @param {Array} creatures - массив объектов врага, координаты которых необходимо менять при расширении карты
- * @returns {Object} updatedTargetCoordinates - новые координаты вскапываемой точки,
- * map - увеличенная карта,
- * hero - новые координаты героя,
- * creatures - массив объектов врагов с новыми координатами
  */
 function expansionMap(targetCoordinate, dx, dy, map, hero, replacement, replacementItems, creatures) {
-    const mapForUpdate = [...map],
-        allEnemyOnMap = [...creatures],
-        elementsForReplace = [...replacementItems],
-        { emptySpace } = elements;
-    let { x: targetCoordinateX, y: targetCoordinateY } = targetCoordinate,
-        { positionX: heroPositionX, positionY: heroPositionY } = hero;
+    const { emptySpace } = elements;
 
     dy.forEach((currentDy, index) => {
-        let checkCoordinateY = targetCoordinateY + currentDy,
-            checkCoordinateX = targetCoordinateX + dx[index];
+        let checkCoordinateY = targetCoordinate.y + currentDy,
+            checkCoordinateX = targetCoordinate.x + dx[index];
 
-        if (mapForUpdate[checkCoordinateY] === undefined) {
-            while (checkCoordinateY < 0 || checkCoordinateY > mapForUpdate.length - 1) {
+        if (map[checkCoordinateY] === undefined) {
+            while (checkCoordinateY < 0 || checkCoordinateY > map.length - 1) {
                 const newAddedLine = [];
 
-                mapForUpdate[heroPositionY].forEach(() => {
+                map[hero.positionY].forEach(() => {
                     newAddedLine.push([emptySpace]);
                 });
                 newAddedLine[checkCoordinateX] = [replacement];
                 if (checkCoordinateY < 0) { // добавление новой линии к карте сверху
-                    mapForUpdate.unshift(newAddedLine); // добавление в начало массива map значения newAddedLine
-                    heroPositionY += 1;
-                    targetCoordinateY += 1;
+                    map.unshift(newAddedLine); // добавление в начало массива map значения newAddedLine
+                    hero.positionY += 1;
+                    targetCoordinate.y += 1;
                     checkCoordinateY += 1;
-                    allEnemyOnMap.forEach((currentEnemy) => {
+                    creatures.forEach((currentEnemy) => {
                         currentEnemy.positionY += 1;
                     });
-                } else if (checkCoordinateY > mapForUpdate.length - 1) { // добавление линии снизу
-                    mapForUpdate.push(newAddedLine);
+                } else if (checkCoordinateY > map.length - 1) { // добавление линии снизу
+                    map.push(newAddedLine);
                 }
             }
-        } else if (mapForUpdate[checkCoordinateY][checkCoordinateX] === undefined) {
-            while (checkCoordinateX < 0 || checkCoordinateX > mapForUpdate[checkCoordinateY].length - 1) {
+        } else if (map[checkCoordinateY][checkCoordinateX] === undefined) {
+            while (checkCoordinateX < 0 || checkCoordinateX > map[checkCoordinateY].length - 1) {
                 if (checkCoordinateX < 0) { // добавление новой клетки к карте слева
-                    mapForUpdate.forEach((currentRow) => {
+                    map.forEach((currentRow) => {
                         currentRow.unshift([emptySpace]); // добавление в начало каждой строки элемента
                     });
-                    allEnemyOnMap.forEach((currentEnemy) => {
+                    creatures.forEach((currentEnemy) => {
                         currentEnemy.positionX += 1;
                     });
-                    mapForUpdate[checkCoordinateY][0][0] = replacement;
-                    heroPositionX += 1;
-                    targetCoordinateX += 1;
+                    map[checkCoordinateY][0][0] = replacement;
+                    hero.positionX += 1;
+                    targetCoordinate.x += 1;
                     checkCoordinateX += 1;
                 } else { // добавление столбца к карте справа
-                    mapForUpdate.forEach((currentRow) => {
+                    map.forEach((currentRow) => {
                         currentRow.push([emptySpace]);
                     });
                 }
             }
-            mapForUpdate[checkCoordinateY][checkCoordinateX][0] = replacement;
+            map[checkCoordinateY][checkCoordinateX][0] = replacement;
         } else {
-            elementsForReplace.forEach((currentReplace) => {
-                if (mapForUpdate[checkCoordinateY][checkCoordinateX][0].icon === currentReplace.icon) {
-                    mapForUpdate[checkCoordinateY][checkCoordinateX][0] = replacement;
+            replacementItems.forEach((currentReplace) => {
+                if (map[checkCoordinateY][checkCoordinateX][0].icon === currentReplace.icon) {
+                    map[checkCoordinateY][checkCoordinateX][0] = replacement;
                 }
             });
         }
     });
-
-    return {
-        updatedTargetCoordinates: {
-            x: targetCoordinateX,
-            y: targetCoordinateY
-        },
-        map: [...mapForUpdate],
-        hero: {
-            ...hero,
-            positionX: heroPositionX,
-            positionY: heroPositionY
-        },
-        creatures: [...allEnemyOnMap]
-    };
 }
 
 export default expansionMap;
--- src/tests/expansionMap.test.js
+++ src/tests/expansionMap.test.js
@@ -2,261 +2,261 @@ import expansionMap from '../functional/expansionMap';
 
 describe('Тесты функции expansionMap', () => {
     it('Корректная замена элемента "Пустое место" на элемент "Стена"', () => {
-        expect(expansionMap(
-            { x: 0, y: 0 },
-            [0],
-            [0],
-            [[[{ "icon": " " }]]],
-            {
+        const inputCoord = { x: 0, y: 0 },
+            outputCoord = { x: 0, y: 0 },
+            inputMap = [[[{ "icon": " " }]]],
+            outputMap = [[[{ "icon": "wall" }]]],
+            inputHero = {
                 "positionX": 0,
                 "positionY": 0
             },
-            { "icon": "wall" },
-            [{ "icon": " " }],
-            [{
-                "positionX": 0,
-                "positionY": 0
-            }]
-        )).toEqual({
-            "updatedTargetCoordinates": { x: 0, y: 0 },
-            "map": [[[{ "icon": "wall" }]]],
-            "hero": {
+            outputHero = {
                 "positionX": 0,
                 "positionY": 0
             },
-            "creatures": [{
+            inputCreatures = [{
                 "positionX": 0,
                 "positionY": 0
-            }]
-        })
+            }],
+            outputCreatures = [{
+                "positionX": 0,
+                "positionY": 0
+            }];
+
+        expansionMap(inputCoord, [0], [0], inputMap, inputHero, { "icon": "wall" }, [{ "icon": " " }], inputCreatures);
+
+        expect(inputCoord).toEqual(outputCoord);
+        expect(inputMap).toEqual(outputMap);
+        expect(inputHero).toEqual(outputHero);
+        expect(inputCreatures).toEqual(outputCreatures);
     });
 
     it('Добавление новой строки карты сверху c генерацией стены', () => {
-        expect(expansionMap(
-            { x: 0, y: 0 },
-            [0],
-            [-1],
-            [[[{ "icon": "floor" }]]],
-            {
-                "positionX": 0,
-                "positionY": 0
-            },
-            { "icon": "wall" },
-            [{ "icon": " " }],
-            [{
-                "positionX": 0,
-                "positionY": 0
-            }]
-        )).toEqual({
-            "updatedTargetCoordinates": { x: 0, y: 1 },
-            "map": [
+        const inputCoord = { x: 0, y: 0 },
+            outputCoord = { x: 0, y: 1 },
+            inputMap = [[[{ "icon": "floor" }]]],
+            outputMap = [
                 [[{ "icon": "wall" }]],
                 [[{ "icon": "floor" }]]
             ],
-            "hero": {
+            inputHero = {
+                "positionX": 0,
+                "positionY": 0
+            },
+            outputHero = {
                 "positionX": 0,
                 "positionY": 1
             },
-            "creatures": [{
+            inputCreatures = [{
+                "positionX": 0,
+                "positionY": 0
+            }],
+            outputCreatures = [{
                 "positionX": 0,
                 "positionY": 1
-            }]
-        })
+            }];
+
+        expansionMap(inputCoord, [0], [-1], inputMap, inputHero, { "icon": "wall" }, [{ "icon": " " }], inputCreatures);
+
+        expect(inputCoord).toEqual(outputCoord);
+        expect(inputMap).toEqual(outputMap);
+        expect(inputHero).toEqual(outputHero);
+        expect(inputCreatures).toEqual(outputCreatures);
     });
 
     it('Добавление новой строки карты снизу c генерацией стены', () => {
-        expect(expansionMap(
-            { x: 0, y: 0 },
-            [0],
-            [1],
-            [[[{ "icon": "floor" }]]],
-            {
-                "positionX": 0,
-                "positionY": 0
-            },
-            { "icon": "wall" },
-            [{ "icon": " " }],
-            [{
-                "positionX": 0,
-                "positionY": 0
-            }]
-        )).toEqual({
-            "updatedTargetCoordinates": { x: 0, y: 0 },
-            "map": [
+        const inputCoord = { x: 0, y: 0 },
+            outputCoord = { x: 0, y: 0 },
+            inputMap = [[[{ "icon": "floor" }]]],
+            outputMap = [
                 [[{ "icon": "floor" }]],
                 [[{ "icon": "wall" }]]
             ],
-            "hero": {
+            inputHero = {
                 "positionX": 0,
                 "positionY": 0
             },
-            "creatures": [{
+            outputHero = {
                 "positionX": 0,
                 "positionY": 0
-            }]
-        })
+            },
+            inputCreatures = [{
+                "positionX": 0,
+                "positionY": 0
+            }],
+            outputCreatures = [{
+                "positionX": 0,
+                "positionY": 0
+            }];
+
+        expansionMap(inputCoord, [0], [1], inputMap, inputHero, { "icon": "wall" }, [{ "icon": " " }], inputCreatures);
+
+        expect(inputCoord).toEqual(outputCoord);
+        expect(inputMap).toEqual(outputMap);
+        expect(inputHero).toEqual(outputHero);
+        expect(inputCreatures).toEqual(outputCreatures);
     });
 
     it('Добавление нового столбца карты справа c генерацией стены', () => {
-        expect(expansionMap(
-            { x: 0, y: 0 },
-            [1],
-            [0],
-            [[[{ "icon": "floor" }]]],
-            {
-                "positionX": 0,
-                "positionY": 0
-            },
-            { "icon": "wall" },
-            [{ "icon": " " }],
-            [{
-                "positionX": 0,
-                "positionY": 0
-            }]
-        )).toEqual({
-            "updatedTargetCoordinates": { x: 0, y: 0 },
-            "map": [[
+        const inputCoord = { x: 0, y: 0 },
+            outputCoord = { x: 0, y: 0 },
+            inputMap = [[[{ "icon": "floor" }]]],
+            outputMap = [[
                 [{ "icon": "floor" }],
                 [{ "icon": "wall" }]
             ]],
-            "hero": {
+            inputHero = {
                 "positionX": 0,
                 "positionY": 0
             },
-            "creatures": [{
+            outputHero = {
                 "positionX": 0,
                 "positionY": 0
-            }]
-        })
+            },
+            inputCreatures = [{
+                "positionX": 0,
+                "positionY": 0
+            }],
+            outputCreatures = [{
+                "positionX": 0,
+                "positionY": 0
+            }];
+
+        expansionMap(inputCoord, [1], [0], inputMap, inputHero, { "icon": "wall" }, [{ "icon": " " }], inputCreatures);
+
+        expect(inputCoord).toEqual(outputCoord);
+        expect(inputMap).toEqual(outputMap);
+        expect(inputHero).toEqual(outputHero);
+        expect(inputCreatures).toEqual(outputCreatures);
     });
 
     it('Добавление нового столбца карты слева c генерацией стены', () => {
-        expect(expansionMap(
-            { x: 0, y: 0 },
-            [-1],
-            [0],
-            [[[{ "icon": "floor" }]]],
-            {
-                "positionX": 0,
-                "positionY": 0
-            },
-            { "icon": "wall" },
-            [{ "icon": " " }],
-            [{
-                "positionX": 0,
-                "positionY": 0
-            }]
-        )).toEqual({
-            "updatedTargetCoordinates": { x: 1, y: 0 },
-            "map": [[
+        const inputCoord = { x: 0, y: 0 },
+            outputCoord = { x: 1, y: 0 },
+            inputMap = [[[{ "icon": "floor" }]]],
+            outputMap = [[
                 [{ "icon": "wall" }],
                 [{ "icon": "floor" }]
             ]],
-            "hero": {
+            inputHero = {
+                "positionX": 0,
+                "positionY": 0
+            },
+            outputHero = {
                 "positionX": 1,
                 "positionY": 0
             },
-            "creatures": [{
+            inputCreatures = [{
+                "positionX": 0,
+                "positionY": 0
+            }],
+            outputCreatures = [{
                 "positionX": 1,
                 "positionY": 0
-            }]
-        })
+            }];
+
+        expansionMap(inputCoord, [-1], [0], inputMap, inputHero, { "icon": "wall" }, [{ "icon": " " }], inputCreatures);
+
+        expect(inputCoord).toEqual(outputCoord);
+        expect(inputMap).toEqual(outputMap);
+        expect(inputHero).toEqual(outputHero);
+        expect(inputCreatures).toEqual(outputCreatures);
     });
 
     it('Добавление новой строки сверху и нового столбца слева c генерацией травы', () => {
-        expect(expansionMap(
-            { x: 0, y: 0 },
-            [-1, +0],
-            [+0, -1],
-            [[[{ "icon": "floor" }]]],
-            {
-                "positionX": 0,
-                "positionY": 0
-            },
-            { "icon": "floor" },
-            [{ "icon": " " }],
-            [{
-                "positionX": 0,
-                "positionY": 0
-            }]
-        )).toEqual({
-            "updatedTargetCoordinates": { x: 1, y: 1 },
-            "map": [
+        const inputCoord = { x: 0, y: 0 },
+            outputCoord = { x: 1, y: 1 },
+            inputMap = [[[{ "icon": "floor" }]]],
+            outputMap = [
                 [[{ "icon": " " }], [{ "icon": "floor" }]],
                 [[{ "icon": "floor" }], [{ "icon": "floor" }]]
             ],
-            "hero": {
+            inputHero = {
+                "positionX": 0,
+                "positionY": 0
+            },
+            outputHero = {
                 "positionX": 1,
                 "positionY": 1
             },
-            "creatures": [{
+            inputCreatures = [{
+                "positionX": 0,
+                "positionY": 0
+            }],
+            outputCreatures = [{
                 "positionX": 1,
                 "positionY": 1
-            }]
-        })
+            }];
+
+        expansionMap(inputCoord, [-1, 0], [0, -1], inputMap, inputHero, { "icon": "floor" }, [{ "icon": " " }], inputCreatures);
+
+        expect(inputCoord).toEqual(outputCoord);
+        expect(inputMap).toEqual(outputMap);
+        expect(inputHero).toEqual(outputHero);
+        expect(inputCreatures).toEqual(outputCreatures);
     });
 
     it('Добавление 2 столбцов справа с генерацией травы', () => {
-        expect(expansionMap(
-            { x: 0, y: 0 },
-            [2],
-            [0],
-            [[[{ "icon": "floor" }]]],
-            {
-                "positionX": 0,
-                "positionY": 0
-            },
-            { "icon": "floor" },
-            [{ "icon": " " }],
-            [{
-                "positionX": 0,
-                "positionY": 0
-            }]
-        )).toEqual({
-            "updatedTargetCoordinates": { x: 0, y: 0 },
-            "map": [
+        const inputCoord = { x: 0, y: 0 },
+            outputCoord = { x: 0, y: 0 },
+            inputMap = [[[{ "icon": "floor" }]]],
+            outputMap = [
                 [[{ "icon": "floor" }], [{ "icon": " " }], [{ "icon": "floor" }]],
             ],
-            "hero": {
+            inputHero = {
                 "positionX": 0,
                 "positionY": 0
             },
-            "creatures": [{
+            outputHero = {
                 "positionX": 0,
                 "positionY": 0
-            }]
-        })
+            },
+            inputCreatures = [{
+                "positionX": 0,
+                "positionY": 0
+            }],
+            outputCreatures = [{
+                "positionX": 0,
+                "positionY": 0
+            }];
+
+        expansionMap(inputCoord, [2], [0], inputMap, inputHero, { "icon": "floor" }, [{ "icon": " " }], inputCreatures);
+
+        expect(inputCoord).toEqual(outputCoord);
+        expect(inputMap).toEqual(outputMap);
+        expect(inputHero).toEqual(outputHero);
+        expect(inputCreatures).toEqual(outputCreatures);
     });
 
     it('Проверка сохранения положения нескольких монстров на карте при добавлении строки слева', () => {
-        expect(expansionMap(
-            { x: 0, y: 0 },
-            [-1],
-            [0],
-            [[[{ "icon": "floor" }]]],
-            {
+        const inputCoord = { x: 0, y: 0 },
+            outputCoord = { x: 1, y: 0 },
+            inputMap = [[[{ "icon": "floor" }]]],
+            outputMap = [
+                [[{ "icon": "floor" }], [{ "icon": "floor" }]],
+            ],
+            inputHero = {
                 "positionX": 0,
                 "positionY": 0
             },
-            { "icon": "floor" },
-            [{ "icon": " " }],
-            [
-                { "positionX": 0, "positionY": 0 },
-                { "positionX": 0, "positionY": 0 }
-            ]
-        )).toEqual({
-            "updatedTargetCoordinates": { x: 1, y: 0 },
-            "map": [
-                [[{ "icon": "floor" }], [{ "icon": "floor" }]],
-            ],
-            "hero": {
+            outputHero = {
                 "positionX": 1,
                 "positionY": 0
             },
-            "creatures": [
+            inputCreatures = [
+                { "positionX": 0, "positionY": 0 },
+                { "positionX": 0, "positionY": 0 }
+            ],
+            outputCreatures = [
                 { "positionX": 1, "positionY": 0 },
                 { "positionX": 1, "positionY": 0 }
-            ]
-        })
+            ];
+
+        expansionMap(inputCoord, [-1], [0], inputMap, inputHero, { "icon": "floor" }, [{ "icon": " " }], inputCreatures);
+
+        expect(inputCoord).toEqual(outputCoord);
+        expect(inputMap).toEqual(outputMap);
+        expect(inputHero).toEqual(outputHero);
+        expect(inputCreatures).toEqual(outputCreatures);
     });
 })
